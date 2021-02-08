8d3495e09ee0c9a7f64edfa287154b452d6c710d
Eric Arroyo <eric.arroyo@softvision.com>
2021-02-08 14:37:17 -0300

    added toggle Log button

--- scenes/SimpleBoard.gd
+++ scenes/SimpleBoard.gd
@@ -2,6 +2,7 @@ extends Control
 
 onready var button_next_phase := $TurnPhase/VBoxContainer/NextPhase
 onready var label_turn_phase := $TurnPhase/VBoxContainer/TurnPhase
+onready var log_panel := $Log
 onready var log_scroll := $Log/ScrollContainer
 onready var log_container := $Log/ScrollContainer/VBoxContainer
 
@@ -28,21 +29,29 @@ func _on_Game_set_state():
 func _on_NextPhase_pressed():
 	game.go_to_next_phase()
 
+var force_scroll_log_to_end := false
+
 func _on_Game_log_message(message: String):
 	var new_label := Label.new()
 	new_label.autowrap = true
 	new_label.text = message
+	
+	force_scroll_log_to_end = true
 	log_container.add_child(new_label)
+	force_scroll_log_to_end = false
 
 var lock_scroll_changed := false
 
 func _on_LogScroll_changed():
-	if lock_scroll_changed:
-		return
-	lock_scroll_changed = true
-	log_scroll.scroll_vertical = 999999
-	lock_scroll_changed = false
+	if force_scroll_log_to_end and not lock_scroll_changed:
+		lock_scroll_changed = true
+		log_scroll.scroll_vertical = 999999
+		lock_scroll_changed = false
 
 func _on_Game_create_animation(animation: AnimatedSprite):
 	add_child(animation)
 	animation.play()
+
+
+func _on_ShowLog_toggled(button_pressed: bool):
+	log_panel.visible = button_pressed
